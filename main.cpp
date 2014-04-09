45f33e27bf1cde278e4d42b92a11f8cb0c823c4c
Menghan Li <lmhhaha@gmail.com>
2014-04-08 20:49:47 -0400

    add query and other query class files

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 
+#include "query.h"
+
 using namespace std;
 
 int main()
 {
-    cout << "hello";
+    cout << "hello" << endl;
 }
---
+++ query.h
@@ -0,0 +1,9 @@
+
+
+template <typename T>
+class Query
+{
+    public:
+        virtual T update_with_new_value(T v);
+};
+
