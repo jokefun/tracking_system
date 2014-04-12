24e860b9bef90c5737809d328a09d4445c50a4e9
Menghan Li <lmhhaha@gmail.com>
2014-04-11 23:24:51 -0400

    test framework and example query

--- query.h
+++ query.h
@@ -1,9 +1,11 @@
-
+#ifndef QUERY_DEFINATION
+#define QUERY_DEFINATION
 
 template <typename T>
 class Query
 {
     public:
-        virtual T update_with_new_value(T v);
+        virtual T update_with_new_value(T v) = 0;
 };
 
+#endif
--- queryTests.cpp
+++ queryTests.cpp
@@ -3,9 +3,11 @@
 #include <vector>
 
 #include "query.h"
+#include "newElementQuery.h"
 
 using namespace std;
 
+// print
 void print(const vector<double>& v)
 {
     vector<double>::const_iterator i;
@@ -15,6 +17,9 @@ void print(const vector<double>& v)
     }
 }
 
+/*
+ * populate random double into vector examples
+ */
 void populateRandomExample(vector<double>& examples, int count)
 {
     random_device generator;
@@ -25,14 +30,77 @@ void populateRandomExample(vector<double>& examples, int count)
     }
 }
 
+void printWhenTestError(vector<double>::iterator b, vector<double>::iterator e,
+        double fromQuery, double fromTestFunc)
+{
+    cout << "data: ";
+    for (vector<double>::iterator i=b; i!=e+1; ++i)
+        cout << *i << " ";
+    cout << "\nquery result: " << fromQuery;
+    cout << "\ntestF result: " << fromTestFunc << endl;
+}
+
+double testFunctionNewElementQuery(vector<double>::iterator b, vector<double>::iterator e)
+{
+    return *e;
+}
+
+double testFunctionTest(vector<double>::iterator b, vector<double>::iterator e)
+{
+    for (vector<double>::iterator i=b; i!=e; i++)
+        cout << *i << " ";
+    cout << endl;
+    return 0;
+}
+
+/*
+ * run over examples using query and testFunction
+ * to test whether query works as expected
+ */
+bool doTest(vector<double>& examples, int window_size,
+        Query<double>* query,
+        double (*testFunction)(vector<double>::iterator, vector<double>::iterator)) // query and function needed
+{
+    if (!query) return false;
+
+    for (vector<double>::iterator i=examples.begin(); i!=examples.end(); ++i)
+    {
+        double fromQuery = query->update_with_new_value(*i);
+
+        vector<double>::iterator b = i-window_size;
+        if (distance(examples.begin(), b) < 0)
+            b = examples.begin();
+        double fromTestFunc = (*testFunction)(b, i);
+
+        if (fromQuery != fromTestFunc)
+        {
+            printWhenTestError(b,i,fromQuery,fromTestFunc);
+            return false;
+        }
+    }
+    return true;
+}
+
 int main()
 {
     /* Query<double>* query[3]; */
     const int count=10;
+    const int window_size=5;
 
     vector<double> examples;
     populateRandomExample(examples, count);
     print(examples);
+
+    NewElementQuery<double> neq;
+
+    if (!doTest(examples, window_size, (Query<double>*)&neq, &testFunctionNewElementQuery))
+    {
+        cout << "test failed\n";
+    }
+    else
+    {
+        cout << "test passed\n";
+    }
 
     return 0;
 }
