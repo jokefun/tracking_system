d0268943cf5c1472cd9fdfea2a0869caa9849fd4
cxb <cxb@ubuntu.(none)>
2014-04-12 19:04:20 -0700

    maxQuery

--- maxQuery.h
+++ maxQuery.h
@@ -2,63 +2,106 @@
 #define MAXQUERY_H_H
 
 #include "query.h"
-#include "myData.h"
 #include <deque>          // std::queue
 
 
 const int WINDOWSIZE = 500;
 
 template <class T>
-class maxQuery:public Query<T>
+class MaxQuery:public Query<T>
 {
-private:
-	T data;
-	int size;
-	std::deque<T> dataWindow;
-	int windowSize;
-public:
-	maxQuery();
-	maxQuery(int windowSize_);
-	~maxQuery();
-	double addNewData(T data_);
-	T getCurMax();
-	T update_with_new_value(T data);
+	private:
+		struct myData
+		{
+			T value;
+			int index;
+		};
+		int size;
+		int index;
+		std::deque<myData> dataWindow;
+		int windowSize;
+		double addNewData(T data_);
+		T getCurMax();
+	public:
+		MaxQuery();
+		MaxQuery(int windowSize_);
+		MaxQuery(const MaxQuery<T>& MQ);
+		MaxQuery(MaxQuery<T>&& MQ);
+		~MaxQuery();
+		T update_with_new_value(T data);
+		MaxQuery & operator=(const MaxQuery &);
 
 };
 
 template<class T>
-maxQuery<T>::~maxQuery()
+MaxQuery<T> & MaxQuery<T>:: operator=(const MaxQuery &MQ)
 {
+	size = MQ.size;
+	index = MQ.index;
+	windowSize = MQ.windowSize;
+	dataWindow = MQ.dataWindow;
+	return *this;
+}
 
+
+template<class T>
+MaxQuery<T>::MaxQuery(MaxQuery<T>&& MQ)
+{
+	size = MQ.size;
+	index = MQ.index;
+	windowSize = MQ.windowSize;
+	dataWindow = MQ.dataWindow;
+}
+
+
+template<class T>
+MaxQuery<T>::~MaxQuery()
+{
+	
 }
 
 template<class T>
-maxQuery<T>::maxQuery(int windowSize_)
+MaxQuery<T>::MaxQuery(const MaxQuery<T>& MQ)
+{
+	size = MQ.size;
+	index = MQ.index;
+	windowSize = MQ.windowSize;
+	dataWindow = MQ.dataWindow;
+}
+
+template<class T>
+MaxQuery<T>::MaxQuery(int windowSize_)
 {
 	size = 0;
+	index = 0;
 	windowSize = windowSize_;
 }
 
 template<class T>
-maxQuery<T>::maxQuery()
+MaxQuery<T>::MaxQuery()
 {
 	size = 0;
+	index = 0;
 	windowSize = WINDOWSIZE;
 }
 
 template<class T>
-double maxQuery<T>::addNewData(T data)
+double MaxQuery<T>::addNewData(T value)
 {
+	index++;
+	struct myData data;
+	data.value = value;
+	data.index = index;
 	if (dataWindow.empty())
 	{
 		dataWindow.push_back(data);
-		return data.getValue();
+		return data.value;
 	}
-	if (dataWindow.front().getIndex() <= data.getIndex() - windowSize)
+	if (dataWindow.front().index <= data.index - windowSize)
 		dataWindow.pop_front();		// out of date
-	if (dataWindow.front().getValue() >= data.getValue())
+	if (dataWindow.front().value >= data.value)
 	{
-		while (dataWindow.back().getValue() < data.getValue())
+		while (dataWindow.back().value < data.value)
 			dataWindow.pop_back();
 		dataWindow.push_back(data);
 	}
@@ -70,19 +113,21 @@ double maxQuery<T>::addNewData(T data)
 		}
 		dataWindow.push_back(data);
 	}
-	return data.getValue();
+	return data.value;
 }
 
 template<class T>
-T maxQuery<T>::getCurMax()
+T MaxQuery<T>::getCurMax()
 {
-	return dataWindow.front();
+	return dataWindow.front().value;
 } 
 
 template<class T>
-T maxQuery<T>::update_with_new_value(T data)
+T MaxQuery<T>::update_with_new_value(T data)
 {
 	addNewData(data);
 	return getCurMax();
 }
+
+
 #endif
